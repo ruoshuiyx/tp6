26d11d5e98d03c299de595e1b091e69f849be703
zhaoyingjie <407593529@qq.com>
2019-04-04 16:22:07 +0800

    AdType 整理完毕

--- app/admin/controller/AdType.php
+++ app/admin/controller/AdType.php
@@ -8,7 +8,7 @@
  *                    :::::::::::           | EMAIL: 407593529@qq.com
  *                 ..:::::::::::'           | QQ: 407593529
  *             '::::::::::::'               | WECHAT: zhaoyingjie4125
- *                .::::::::::               | DATETIME: 2019/03/04
+ *                .::::::::::               | DATETIME: 2019/04/04
  *           '::::::::::::::..
  *                ..::::::::::::.
  *              ``::::::::::::::::
@@ -24,11 +24,13 @@
  * +----------------------------------------------------------------------
  */
 namespace app\admin\controller;
-use think\facade\Request;
-use think\Db;
 
-//实例化默认模型
 use app\common\model\AdType as M;
+
+use think\facade\Config;
+use think\facade\Db;
+use think\facade\Request;
+use think\facade\View;
 
 class AdType extends Base
 {
@@ -39,43 +41,48 @@ class AdType extends Base
     {
         //条件筛选
         $keyword = Request::param('keyword');
-        $this->view->assign('keyword',$keyword);
         //全局查询条件
         $where=[];
         if(!empty($keyword)){
             $where[]=['name|description', 'like', '%'.$keyword.'%'];
         }
         //显示数量
-        $pageSize = Request::param('page_size') ? Request::param('page_size') : config('page_size');
-        $this->view->assign('pageSize', page_size($pageSize));
+        $pageSize = Request::param('page_size') ? Request::param('page_size') : Config::get('app.page_size');
 
         //调取列表
         $list = M::where($where)
             ->order('sort ASC,id DESC')
             ->paginate($pageSize,false,['query' => request()->param()]);
         $page = $list->render();
-        $this->view->assign('page' , $page);
-        $this->view->assign('list' , $list);
-        $this->view->assign('empty', empty_list(9));
-        return $this->view->fetch();
+        $view = [
+            'keyword'=>$keyword,
+            'pageSize' => page_size($pageSize),
+            'page' => $page,
+            'list' => $list,
+            'empty'=> empty_list(12),
+        ];
+        View::assign($view);
+        return View::fetch();
     }
 
     //添加
     public function add(){
-        $this->view->assign('info',null);
-        return $this->view->fetch();
+        $view = [
+            'info'   => null
+        ];
+        View::assign($view);
+        return View::fetch();
     }
 
     //添加保存
     public function addPost(){
-        $data = Request::param();
+        $data = Request::except(['file']);
         $result = $this->validate($data,$this->validate);
         if (true !== $result) {
             // 验证失败 输出错误信息
             $this->error($result);
         }else{
-            $m = new M();
-            $result =  $m->addPost($data);
+            $result =  M::addPost($data);
             if($result['error']){
                 $this->error($result['msg']);
             }else{
@@ -86,26 +93,24 @@ class AdType extends Base
 
     //修改
     public function edit(){
-        $m = new M();
         $id = Request::param('id');
-        if( empty($id) ){
-            return ['error'=>1,'msg'=>'ID不存在'];
-        }
-        $info = $m->edit($id);
-        $this->view->assign('info', $info);
-        return $this->view->fetch('add');
+        $info = M::edit($id);
+        $view =[
+            'info'   => $info,
+        ];
+        View::assign($view);
+        return View::fetch('add');
     }
 
     //修改保存
     public function editPost(){
-        $data = Request::param();
+        $data = Request::except(['file']);
         $result = $this->validate($data,$this->validate);
         if (true !== $result) {
             // 验证失败 输出错误信息
             $this->error($result);
         }else{
-            $m = new M();
-            $result = $m->editPost($data);
+            $result = M::editPost($data);
             if($result['error']){
                 $this->error($result['msg']);
             }else{
@@ -117,52 +122,33 @@ class AdType extends Base
     //删除
     public function del(){
         if(Request::isPost()) {
-            $id = Request::post('id');
-            if( empty($id) ){
-                return ['error'=>1,'msg'=>'ID不存在'];
-            }
-            $m = new M();
-            return $m->del($id);
+            $id = Request::param('id');
+            return M::del($id);
         }
     }
 
     //批量删除
     public function selectDel(){
         if(Request::isPost()) {
-            $id = Request::post('id');
-            if (empty($id)) {
-                return ['error'=>1,'msg'=>'ID不存在'];
-            }
-            $m = new M();
-            return $m->selectDel($id);
+            $id = Request::param('id');
+            return M::selectDel($id);
         }
-
     }
 
     //排序
     public function sort(){
         if(Request::isPost()){
             $data = Request::param();
-            if (empty($data['id'])){
-                return ['error'=>1,'msg'=>'ID不存在'];
-            }
-            $m = new M();
-            return $m->sort($data);
+            return M::sort($data);
         }
     }
 
     //状态
     public function state(){
         if(Request::isPost()){
-            $id = Request::post('id');
-            if (empty($id)){
-                return ['error'=>1,'msg'=>'ID不存在'];
-            }
-            $m = new M();
-            return $m->state($id);
+            $id = Request::param('id');
+            return M::state($id);
         }
-
-
     }
 
 }
