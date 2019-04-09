2e16fa73d6eebeed0eaa1fa0b43c422ef7ed4f3d
zhaoyingjie <407593529@qq.com>
2019-04-09 14:15:37 +0800

    框架提交

---
+++ vendor/topthink/framework/src/think/Cookie.php
@@ -0,0 +1,165 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2019 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+declare (strict_types = 1);
+
+namespace think;
+
+class Cookie
+{
+    /**
+     * 配置参数
+     * @var array
+     */
+    protected $config = [
+        // cookie 名称前缀
+        'prefix'   => '',
+        // cookie 保存时间
+        'expire'   => 0,
+        // cookie 保存路径
+        'path'     => '/',
+        // cookie 有效域名
+        'domain'   => '',
+        //  cookie 启用安全传输
+        'secure'   => false,
+        // httponly设置
+        'httponly' => false,
+    ];
+
+    /**
+     * Cookie写入数据
+     * @var array
+     */
+    protected $cookie = [];
+
+    /**
+     * 构造方法
+     * @access public
+     */
+    public function __construct(array $config = [])
+    {
+        $this->config = array_merge($this->config, array_change_key_case($config));
+    }
+
+    public static function __make(Config $config)
+    {
+        return new static($config->get('cookie'));
+    }
+
+    /**
+     * 获取cookie保存数据
+     * @access public
+     * @return array
+     */
+    public function getCookie(): array
+    {
+        return $this->cookie;
+    }
+
+    /**
+     * 设置或者获取cookie作用域（前缀）
+     * @access public
+     * @param  string $prefix
+     * @return string|void
+     */
+    public function prefix(string $prefix = '')
+    {
+        if (empty($prefix)) {
+            return $this->config['prefix'];
+        }
+
+        $this->config['prefix'] = $prefix;
+    }
+
+    /**
+     * Cookie 设置
+     *
+     * @access public
+     * @param  string $name  cookie名称
+     * @param  string $value cookie值
+     * @param  mixed  $option 可选参数 可能会是
+     * @return void
+     */
+    public function set(string $name, string $value, $option = null): void
+    {
+        // 参数设置(会覆盖黙认设置)
+        if (!is_null($option)) {
+            if (is_numeric($option)) {
+                $option = ['expire' => $option];
+            }
+
+            $config = array_merge($this->config, array_change_key_case($option));
+        } else {
+            $config = $this->config;
+        }
+
+        $expire = !empty($config['expire']) ? time() + intval($config['expire']) : 0;
+
+        $this->setCookie($config['prefix'] . $name, (string) $value, $expire, $config);
+    }
+
+    /**
+     * Cookie 保存
+     *
+     * @access public
+     * @param  string $name  cookie名称
+     * @param  mixed  $value cookie值
+     * @param  int    $expire 有效期
+     * @param  array  $option 可选参数
+     * @return void
+     */
+    protected function setCookie(string $name, string $value, int $expire, array $option = []): void
+    {
+        $this->cookie[$name] = [$value, $expire, $option];
+    }
+
+    /**
+     * 永久保存Cookie数据
+     * @access public
+     * @param  string $name  cookie名称
+     * @param  string $value cookie值
+     * @param  mixed  $option 可选参数 可能会是 null|integer|string
+     * @return void
+     */
+    public function forever(string $name, string $value = '', $option = null): void
+    {
+        if (is_null($option) || is_numeric($option)) {
+            $option = [];
+        }
+
+        $option['expire'] = 315360000;
+
+        $this->set($name, $value, $option);
+    }
+
+    /**
+     * Cookie删除
+     * @access public
+     * @param  string      $name cookie名称
+     * @param  string|null $prefix cookie前缀
+     * @return void
+     */
+    public function delete(string $name, string $prefix = null): void
+    {
+        $prefix = $prefix ?: $this->config['prefix'];
+
+        $this->setCookie($prefix . $name, '', time() - 3600, $this->config);
+    }
+
+    public function save(): void
+    {
+        foreach ($this->cookie as $name => $val) {
+            list($value, $expire, $option) = $val;
+
+            setcookie($name, $value, $expire, $option['path'], $option['domain'], $option['secure'] ? true : false, $option['httponly'] ? true : false);
+        }
+    }
+
+}
