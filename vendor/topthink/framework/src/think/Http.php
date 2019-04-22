a3a01608e5dd768e73aeb7d63d53004bbc636561
zhaoyingjie <407593529@qq.com>
2019-04-22 16:38:22 +0800

    框架更新

--- vendor/topthink/framework/src/think/Http.php
+++ vendor/topthink/framework/src/think/Http.php
@@ -12,6 +12,7 @@ declare (strict_types = 1);
 
 namespace think;
 
+use Closure;
 use think\exception\Handle;
 use think\exception\HttpException;
 use Throwable;
@@ -300,7 +301,12 @@ class Http
                 $name = current(explode('/', $path));
 
                 if (isset($map[$name])) {
-                    $appName = $map[$name];
+                    if ($map[$name] instanceof Closure) {
+                        $result  = call_user_func_array($map[$name], [$this]);
+                        $appName = $result ?: $name;
+                    } else {
+                        $appName = $map[$name];
+                    }
                 } elseif ($name && false !== array_search($name, $map)) {
                     throw new HttpException(404, 'app not exists:' . $name);
                 } else {
