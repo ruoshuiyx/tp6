e358496f661df0e4b6145c65fc07023fcbc874e6
zhaoyingjie <407593529@qq.com>
2019-04-28 11:53:11 +0800

    碎片管理代码优化

--- app/common/model/Debris.php
+++ app/common/model/Debris.php
@@ -25,10 +25,19 @@
  */
 namespace app\common\model;
 
+use think\facade\Request;
+
 class Debris extends Base
 {
     // 定义时间戳字段名
     protected $createTime = 'create_time';
     protected $updateTime = 'update_time';
 
+    // 获取列表
+    public static function getList($where=array(),$pageSize,$order=['sort','id'=>'desc']){
+        $list = self::where($where)
+            ->order($order)
+            ->paginate($pageSize,false,['query' => Request::param()]);
+        return $list;
+    }
 }
