23bcd41a2702846b9feed4c852bc3d3936e82bd4
zhaoyingjie <407593529@qq.com>
2019-05-14 11:49:13 +0800

    细节规范

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -164,12 +164,12 @@ class Index extends Base
                 // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
                 $url =  "/uploads/".$info->getSaveName();
                 $url = str_replace("\\","/",$url);
-                echo $url;
+                return $url;
                 // 输出 42a79759f284b767dfcb2a0197904287.jpg
                 //echo $info->getFilename();
             }else{
                 // 上传失败获取错误信息
-                echo $file->getError();
+                return $file->getError();
             }
 
         }
