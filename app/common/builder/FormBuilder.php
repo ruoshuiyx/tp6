5a174daba429ce7fe59f6a67b9f99f3e1e8fc950
zhaoyingjie <407593529@qq.com>
2019-07-31 10:52:06 +0800

    FormBuilder 增加时间字段（采用 Time Picker），删除多余文件

--- app/common/builder/FormBuilder.php
+++ app/common/builder/FormBuilder.php
@@ -454,6 +454,42 @@ class FormBuilder
     }
 
     /**
+     * 添加时间
+     * @param string $name        字段名称
+     * @param string $title       字段别名
+     * @param string $tips        提示信息
+     * @param string $default     默认值
+     * @param string $format      时间格式
+     * @param string $extra_attr  额外属性
+     * @param string $extra_class 额外css类名
+     * @param string $placeholder 占位符
+     * @param bool   $required    是否必填
+     * @return $this|array
+     */
+    public function addTime($name = '', $title = '', $tips = '', $default = '', $format = '', $extra_attr = '', $extra_class = '', $placeholder = '', $required = false)
+    {
+        $item = [
+            'type'        => 'time',
+            'name'        => $name,
+            'title'       => $title,
+            'tips'        => $tips,
+            'value'       => $default,
+            'format'      => $format == '' ? 'HH:mm:ss' : $format,
+            'extra_attr'  => $extra_attr,
+            'extra_class' => $extra_class,
+            'placeholder' => !empty($placeholder) ? $placeholder : '请选择或输入' . $title,
+            'required'    => $required,
+        ];
+
+        if ($this->_is_group) {
+            return $item;
+        }
+
+        $this->_vars['form_items'][] = $item;
+        return $this;
+    }
+
+    /**
      * 一次性添加多个表单项
      * @param array $items 表单项
      * @return $this
