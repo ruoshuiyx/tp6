9255b2cccb5b24a560676437d03bc96b780b3faf
zhaoyingjie <407593529@qq.com>
2020-03-10 09:02:52 +0800

    修改ckeditor ，缩进功能由40px变更为text-indent: 2em

--- public/static/plugins/ckeditor/config.js
+++ public/static/plugins/ckeditor/config.js
@@ -15,19 +15,24 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'about', groups: [ 'about' ] }
 	];
 
-	config.removeButtons = 'Save,NewPage,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Undo,Redo,Find,Replace,SelectAll,Scayt,Form,Checkbox,TextField,Radio,Textarea,Select,Button,ImageButton,HiddenField,Subscript,Superscript,CreateDiv,Blockquote,Outdent,Indent,BidiLtr,BidiRtl,Language,Anchor,Flash,Smiley,SpecialChar,PageBreak,Iframe,About,ShowBlocks,CopyFormatting,Styles,Format';
-	
+	config.removeButtons = 'Save,NewPage,Preview,Cut,Copy,Paste,PasteText,PasteFromWord,Undo,Redo,Find,Replace,SelectAll,Scayt,Form,Checkbox,TextField,Radio,Textarea,Select,Button,ImageButton,HiddenField,CreateDiv,Blockquote,Language,Anchor,Flash,Smiley,SpecialChar,Iframe,ShowBlocks,About,Styles,Subscript,Superscript';
+
 	// 上传图片路径
     config.filebrowserImageUploadUrl = "../index/upload?from=ckeditor";
 	
-	//默认不带高度
+	// 默认不带高度
 	config.disallowedContent = 'img{width,height};img[width,height]';
 	
-	//字体
+	// 字体
 	config.font_names = '微软雅黑/Microsoft YaHei;宋体/SimSun;新宋体/NSimSun;仿宋/FangSong;楷体/KaiTi;黑体/SimHei;' + config.font_names;
 	
-	//关闭内容过滤器
+	// 关闭内容过滤器
 	config.allowedContent = true;
+	
+	// 缩进
+	config.indentOffset = 2;
+	config.indentUnit = 'em';
+	config.getComputedStyle = 2;
 	
 	//config.fontSize_defaultLabel = '14px';
 };
