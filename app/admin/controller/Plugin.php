7f7cf7faacdb5eedccdaebaa7080a595e1f4d1ba
zhaoyingjie <407593529@qq.com>
2020-03-13 17:56:20 +0800

    完善插件模块

--- app/admin/controller/Plugin.php
+++ app/admin/controller/Plugin.php
@@ -31,7 +31,8 @@ use think\facade\View;
 // 表单和表格构建器
 use app\common\builder\FormBuilder;
 use app\common\builder\TableBuilder;
-
+// 插件服务
+use app\admin\facade\ThinkAddons;
 
 class Plugin extends Base
 {
@@ -41,7 +42,7 @@ class Plugin extends Base
         // 搜索
         if (Request::param('getList') == 1) {
             // 获取插件列表
-            $list = $this->getPlugins();
+            $list = ThinkAddons::localAddons();
             // 渲染输出
             $result = [
                 'total'        => count($list),
@@ -75,39 +76,21 @@ class Plugin extends Base
     // 插件配置信息预览
     public function config(string $name)
     {
-        $path = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name;
-        if (!is_dir($path)) {
-            $this->error('未发现该插件，请先安装');
-        }
-        // 实例化插件
-        $object = $this->getInstance($name);
-        if ($object) {
-            // 获取插件基础信息
-            $info = $object->getInfo();
-            if (!$info) {
-                $this->error('未找到该插件的信息');
-            }
-            // 获取插件配置信息
-            $config = $this->getConfig($name);
-
-            // 如果插件自带配置模版的话加载插件自带的，否则调用表单构建器
-            $file = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'config.html';
-            if (file_exists($file)) {
-                View::assign([
-                    'plugin' => $info,
-                    'config' => $config
-                ]);
-                return View::fetch($file);
-            } else {
-                $columns = $this->makeAddColumns($config);
-                return FormBuilder::getInstance()
-                    ->setFormUrl(url('configSave'))
-                    ->addHidden('id', $name)
-                    ->addFormItems($columns)
-                    ->fetch();
-            }
+        $config = ThinkAddons::config($name);
+        // 如果插件自带配置模版的话加载插件自带的，否则调用表单构建器
+        $file = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'config.html';
+        if (file_exists($file)) {
+            View::assign([
+                'config' => $config
+            ]);
+            return View::fetch($file);
         } else {
-            $this->error('插件实例化失败');
+            $columns = $this->makeAddColumns($config);
+            return FormBuilder::getInstance()
+                ->setFormUrl(url('configSave'))
+                ->addHidden('id', $name)
+                ->addFormItems($columns)
+                ->fetch();
         }
     }
 
@@ -115,114 +98,22 @@ class Plugin extends Base
     public function configSave()
     {
         if (Request::isPost()) {
-            $data = Request::except(['file'], 'post');
-            $path = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $data['id'];
-            if (!is_dir($path)) {
-                $this->error('未发现该插件，请先安装');
+            $result = ThinkAddons::configPost(Request::except(['file'], 'post'));
+            if ($result['code'] == 1) {
+                $this->success($result['msg'], 'index');
+            } else {
+                $this->error($result['msg']);
             }
-            // 实例化插件
-            $object = $this->getInstance($data['id']);
-            if ($object) {
-                // 获取插件基础信息
-                $info = $object->getInfo();
-                if (!$info) {
-                    $this->error('未找到该插件的基础信息');
-                }
-                // 获取插件配置信息
-                $config = $this->getConfig($data['id']);
-                if ($data) {
-                    foreach ($config as $k => $v) {
-                        if (isset($data[$k])) {
-                            $value = is_array($data[$k]) ? implode(',', $data[$k]) : ($data[$k] ?? $v['value']);
-                            $config[$k]['value'] = $value;
-                        }
-                    }
-                }
-                // 更新配置文件
-                $result = $this->setPluginConfig($data['id'], $config);
-                if ($result['code'] == 0) {
-                    $this->success('插件信息保存成功!', 'index');
-                } else {
-                    $this->error($result['code']);
-                }
-            }
-
         }
-
     }
 
     // 更改插件状态 [安装/卸载]
     public function state(string $id)
     {
-        $path = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $id;
-        if (!is_dir($path)) {
-            $this->error('未发现该插件');
-        }
-        // 实例化插件
-        $object = $this->getInstance($id);
-        // 获取插件基础信息
-        $info = $object->getInfo();
-        if (!$info) {
-            $this->error('未找到该插件的信息');
-        }
-        if ($info['status'] == 1) {
-            // 卸载
-            if (false !== $object->uninstall()) {
-                $info['status'] = 0;
-                $result = $this->setPluginIni($id, $info);
-                if ($result['code'] == 0) {
-                    $this->success('卸载成功!');
-                } else {
-                    $this->error($result['code']);
-                }
-            } else {
-                $this->error('插件卸载失败');
-            }
-        } else {
-            // 安装
-            /*// 读取插件基类的方法
-            $base = get_class_methods("\\think\\Addons");
-            // 读取出当前插件的方法
-            $methods = (array)get_class_methods($object);
-            // 跟插件基类方法做比对，得到差异结果
-            $hooks = array_diff($methods, $base);*/
-            if (false !== $object->install()) {
-                $info['status'] = 1;
-                $result = $this->setPluginIni($id, $info);
-                if ($result['code'] == 0) {
-                    $this->success('安装成功!');
-                } else {
-                    $this->error($result['msg']);
-                }
-            } else {
-                $this->error('插件安装失败');
-            }
-        }
+        return ThinkAddons::state($id);
     }
 
     // =========================================
-
-    // 获取插件实例
-    private function getInstance($file)
-    {
-        $class = "\\addons\\{$file}\\Plugin";
-        if (class_exists($class)) {
-            // 容器类的工作由think\Container类完成，但大多数情况我们只需要通过app助手函数或者think\App类即可容器操作
-            return app($class);
-        }
-        return false;
-    }
-
-    // 获取完整配置列表[config.php]
-    private function getConfig(string $name)
-    {
-        $file = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'config.php';
-        if (file_exists($file)) {
-            return include $file;
-        } else {
-            return false;
-        }
-    }
 
     // 生成表单信息
     private function makeAddColumns(array $config)
@@ -423,118 +314,4 @@ class Plugin extends Base
         return $columns;
     }
 
-    /**
-     * 获得本地插件列表
-     * 目前只获取本地插件，后期会扩展为获取线上插件，然后通过比对的方式判断是否需要在安装的时候先下载插件
-     * @return array
-     */
-    private function getPlugins()
-    {
-        $plugins = scandir(app()->getRootPath() . 'addons');
-        $list = [];
-        foreach ($plugins as $name) {
-            if ($name === '.' or $name === '..')
-                continue;
-            if (is_file(app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name))
-                continue;
-            $addonDir = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR;
-            if (!is_dir($addonDir))
-                continue;
-
-            $object = $this->getInstance($name);
-            if ($object) {
-                // 获取插件基础信息
-                $info = $object->getInfo();
-                $list[] = $info;
-            }
-        }
-        return $list;
-    }
-
-    /**
-     * 更新插件的ini文件
-     * @param string $name 插件名
-     * @param array $array
-     * @return boolean
-     * @throws Exception
-     */
-    private function setPluginIni($name, $array)
-    {
-        $file = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'info.ini';
-        if (!$this->checkFileWritable($file)) {
-            return [
-                'code' => 1,
-                'msg' => '文件没有写入权限',
-            ];
-        }
-        // 拼接要写入的数据
-        $str = '';
-        foreach ($array as $k => $v) {
-            $str .= $k . " = " . $v . "\n";
-        }
-        if ($handle = fopen($file, 'w')) {
-            fwrite($handle, $str);
-            fclose($handle);
-        } else {
-            return [
-                'code' => 1,
-                'msg'  => '文件没有写入权限',
-            ];
-        }
-        return [
-            'code' => 0,
-            'msg' => '文件写入完毕',
-        ];
-    }
-
-    /**
-     * 更新插件的配置文件
-     * @param string $name 插件名
-     * @param array $array
-     * @return boolean
-     * @throws Exception
-     */
-    private function setPluginConfig($name, $array)
-    {
-        $file = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'config.php';
-        if (!$this->checkFileWritable($file)) {
-            return [
-                'code' => 1,
-                'msg' => '文件没有写入权限',
-            ];
-        }
-        if ($handle = fopen($file, 'w')) {
-            fwrite($handle, "<?php\n\n" . "return " . var_export($array, TRUE) . ";\n");
-            fclose($handle);
-        } else {
-            return [
-                'code' => 1,
-                'msg'  => '文件没有写入权限',
-            ];
-        }
-        return [
-            'code' => 0,
-            'msg' => '文件写入完毕',
-        ];
-    }
-
-    /**
-     * 判断文件或目录是否可写
-     * @param    string $file 文件或目录
-     * @return    bool
-     */
-    private function checkFileWritable($file)
-    {
-        if (is_dir($file)) {
-            // 判断目录是否可写
-            return is_writable($file);
-        } elseif (file_exists($file)) {
-            // 文件存在则判断文件是否可写
-            return is_writable($file);
-        } else {
-            // 文件不存在则判断当前目录是否可写
-            $file = pathinfo($file, PATHINFO_DIRNAME);
-            return is_writable($file);
-        }
-    }
 }
---
+++ app/admin/service/ThinkAddons.php
@@ -0,0 +1,414 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ * | 插件服务类
+ * +----------------------------------------------------------------------
+ * test // 插件名称
+ * ├── app           // 此文件夹中所有文件会覆盖到根目录的/app文件夹
+ * ├── public        // 此文件夹中所有文件会覆盖到根目录的/public文件夹
+ * ├── controller    // 此文件夹为插件控制器目录
+ * ├── model         // 此文件夹为插件模型目录
+ * ├── view          // 此文件夹为插件视图目录
+ * ├── Plugin.php    // 此文件为插件核心安装卸载控制器,必需存在,且名称不可以变动
+ * ├── config.php    // 插件配置文件,后台插件管理中配置中读取和保存的就是这个文件，必须存在，且名称不可以变动
+ * ├── LICENSE       // 版权文件
+ * ├── info.ini      // 插件信息文件,用于保存插件基本信息，不存在则读取Plugin中的配置
+ * └── install.sql   // 插件数据库安装文件,此文件仅在插件安装时会进行导入，如重复安装可能会导致报错
+ */
+namespace app\admin\service;
+
+use think\Facade;
+use think\facade\Config;
+use think\facade\Db;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class ThinkAddons
+{
+    // 插件目录
+    protected $addonsPath = '';
+
+    // 插件安装需要复制的文件夹
+    protected $copyDirs = [];
+
+    // 构造方法
+    public function __construct()
+    {
+        $this->addonsPath = app()->getRootPath() . 'addons';
+        $this->copyDirs = [
+            'app',    // 此文件夹中所有文件会覆盖到根目录的/app文件夹
+            'public', // 此文件夹中所有文件会覆盖到根目录的/public文件夹
+        ];
+    }
+
+    // 获得本地插件列表 [目前只获取本地插件，后期会扩展为获取线上插件]
+    public function localAddons()
+    {
+        $plugins = scandir($this->addonsPath);
+        $list = [];
+        foreach ($plugins as $name) {
+            if ($name === '.' or $name === '..')
+                continue;
+            if (is_file($this->addonsPath . DIRECTORY_SEPARATOR . $name))
+                continue;
+            $addonDir = $this->addonsPath . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR;
+            if (!is_dir($addonDir))
+                continue;
+
+            $object = $this->getInstance($name);
+            if ($object) {
+                // 获取插件基础信息
+                $info = $object->getInfo();
+                $list[] = $info;
+            }
+        }
+        return $list;
+    }
+
+    // 获取插件信息
+    public function config(string $name)
+    {
+        $check = $this->check($name);
+        if ($check !== true) {
+            return [
+                'code' => 0,
+                'msg' => $check
+            ];
+        }
+        return $this->getConfig($name);
+    }
+
+    // 保存插件信息
+    public function configPost(array $data = [])
+    {
+        $check = $this->check($data['id']);
+        if ($check !== true) {
+            return [
+                'code' => 0,
+                'msg' => $check
+            ];
+        }
+        // 实例化插件
+        $object = $this->getInstance($data['id']);
+        if ($object) {
+
+            // 获取插件配置信息
+            $config = $this->getConfig($data['id']);
+            if ($data) {
+                foreach ($config as $k => $v) {
+                    if (isset($data[$k])) {
+                        $value = is_array($data[$k]) ? implode(',', $data[$k]) : ($data[$k] ?? $v['value']);
+                        $config[$k]['value'] = $value;
+                    }
+                }
+            }
+            // 更新配置文件
+            $result = $this->setPluginConfig($data['id'], $config);
+            if ($result['code'] == 1) {
+                return [
+                    'code' => 1,
+                    'msg'  => '保存成功!'
+                ];
+            } else {
+                return [
+                    'code' => 0,
+                    'msg'  => $result['msg']
+                ];
+            }
+        } else {
+            return [
+                'code' => 0,
+                'msg' => '插件实例化失败'
+            ];
+        }
+    }
+
+    // 执行安装或卸载
+    public function state(string $name)
+    {
+        $check = $this->check($name);
+        if ($check !== true) {
+            return [
+                'code' => 0,
+                'msg' => $check
+            ];
+        }
+        // 实例化插件
+        $object = $this->getInstance($name);
+        // 获取插件基础信息
+        $info = $object->getInfo();
+        if (!$info) {
+            return [
+                'code' => 0,
+                'msg'  => '未找到该插件的信息'
+            ];
+        } else {
+            if ($info['status'] == 1) {
+                // 卸载
+                return $this->uninstall($name);
+            } else {
+                // 安装
+                return $this->install($name);
+            }
+        }
+    }
+
+    // 安装插件
+    public function install(string $name)
+    {
+        // 实例化插件
+        $object = $this->getInstance($name);
+        // 获取插件基础信息
+        $info = $object->getInfo();
+
+        if (false !== $object->install()) {
+            $info['status'] = 1;
+            try {
+// 更新或创建插件的ini文件
+                $result = $this->setPluginIni($name, $info);
+                if ($result['code'] == 0) {
+                    return [
+                        'code' => 1,
+                        'msg'  => $result['msg'],
+                    ];
+                }
+                // 复制文件
+                $this->copyDir($name);
+                // 导入SQL
+                $this->importsql($name);
+            } catch (\Exception $e) {
+                return [
+                    'code' => 0,
+                    'msg'  => '安装失败：' . $e->getMessage(),
+                ];
+            }
+        } else {
+            return [
+                'code' => 0,
+                'msg'  => '插件实例化失败',
+            ];
+        }
+        return [
+            'code' => 1,
+            'msg'  => '插件安装成功',
+        ];
+    }
+
+    // 卸载插件
+    public function uninstall(string $name)
+    {
+        // 实例化插件
+        $object = $this->getInstance($name);
+        // 获取插件基础信息
+        $info = $object->getInfo();
+
+        if (false !== $object->install()) {
+            $info['status'] = 0;
+            // 更新或创建插件的ini文件
+            $result = $this->setPluginIni($name, $info);
+            if ($result['code'] == 0) {
+                return [
+                    'code' => 0,
+                    'msg'  => $result['msg'],
+                ];
+            } else {
+                return [
+                    'code' => 1,
+                    'msg'  => '插件卸载成功',
+                ];
+            }
+        } else {
+            return [
+                'code' => 0,
+                'msg'  => '插件实例化失败',
+            ];
+        }
+    }
+
+    // ===========================================
+
+    // 验证插件是否完整
+    private function check(string $name)
+    {
+        if (!is_dir($this->addonsPath . DIRECTORY_SEPARATOR . $name)) {
+            return '未发现该插件,请先下载并放入到addons目录中';
+        }
+        return true;
+    }
+
+    // 获取插件实例
+    private function getInstance(string $file)
+    {
+        $class = "\\addons\\{$file}\\Plugin";
+        if (class_exists($class)) {
+            // 容器类的工作由think\Container类完成，但大多数情况我们只需要通过app助手函数或者think\App类即可容器操作
+            return app($class);
+        }
+        return false;
+    }
+
+    // 获取完整配置列表[config.php]
+    private function getConfig(string $name)
+    {
+        $file = app()->getRootPath() . 'addons' . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'config.php';
+        if (file_exists($file)) {
+            return include $file;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 更新插件的配置文件
+     * @param string $name 插件名
+     * @param array $array
+     * @return boolean
+     * @throws Exception
+     */
+    private function setPluginConfig(string $name, array $array = [])
+    {
+        $file = $this->addonsPath . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'config.php';
+        if (!$this->checkFileWritable($file)) {
+            return [
+                'code' => 0,
+                'msg' => '文件没有写入权限',
+            ];
+        }
+        if ($handle = fopen($file, 'w')) {
+            fwrite($handle, "<?php\n\n" . "return " . var_export($array, TRUE) . ";\n");
+            fclose($handle);
+        } else {
+            return [
+                'code' => 0,
+                'msg'  => '文件没有写入权限',
+            ];
+        }
+        return [
+            'code' => 1,
+            'msg' => '文件写入完毕',
+        ];
+    }
+
+    /**
+     * 更新插件的ini文件
+     * @param string $name 插件名
+     * @param array $array
+     * @return boolean
+     * @throws Exception
+     */
+    private function setPluginIni(string $name, array $array = [])
+    {
+        $file = $this->addonsPath . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'info.ini';
+        if (!$this->checkFileWritable($file)) {
+            return [
+                'code' => 0,
+                'msg' => '文件没有写入权限',
+            ];
+        }
+        // 拼接要写入的数据
+        $str = '';
+        foreach ($array as $k => $v) {
+            $str .= $k . " = " . $v . "\n";
+        }
+        if ($handle = fopen($file, 'w')) {
+            fwrite($handle, $str);
+            fclose($handle);
+        } else {
+            return [
+                'code' => 0,
+                'msg'  => '文件没有写入权限',
+            ];
+        }
+        return [
+            'code' => 1,
+            'msg' => '文件写入完毕',
+        ];
+    }
+
+    /**
+     * 判断文件或目录是否可写
+     * @param    string $file 文件或目录
+     * @return    bool
+     */
+    private function checkFileWritable(string $file)
+    {
+        if (is_dir($file)) {
+            // 判断目录是否可写
+            return is_writable($file);
+        } elseif (file_exists($file)) {
+            // 文件存在则判断文件是否可写
+            return is_writable($file);
+        } else {
+            // 文件不存在则判断当前目录是否可写
+            $file = pathinfo($file, PATHINFO_DIRNAME);
+            return is_writable($file);
+        }
+    }
+
+    // 导入SQL
+    private function importsql(string $name)
+    {
+        $sqlFile = $this->addonsPath . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR . 'install.sql';
+        if (is_file($sqlFile)) {
+            $lines = file($sqlFile);
+            $templine = '';
+            foreach ($lines as $line) {
+                if (substr($line, 0, 2) == '--' || $line == '' || substr($line, 0, 2) == '/*')
+                    continue;
+
+                $templine .= $line;
+                if (substr(trim($line), -1, 1) == ';') {
+                    // 不区分大小写替换前缀
+                    $templine = str_ireplace('__PREFIX__', Config::get('database.connections.mysql.prefix'), $templine);
+                    // 忽略数据库中已经存在的数据
+                    $templine = str_ireplace('INSERT INTO ', 'INSERT IGNORE INTO ', $templine);
+                    try {
+                        Db::execute($templine);
+                    } catch (\PDOException $e) {
+                        //$e->getMessage();
+                    }
+                    $templine = '';
+                }
+            }
+        }
+        return true;
+    }
+
+    // 安装时复制文件
+    private function copyDir(string $name)
+    {
+        $addonDir = $this->addonsPath . DIRECTORY_SEPARATOR . $name . DIRECTORY_SEPARATOR;
+        foreach ($this->copyDirs as $k => $dir) {
+            if (is_dir($addonDir . $dir)) {
+                $this->copydirs($addonDir . $dir, app()->getRootPath() . $dir);
+            }
+        }
+    }
+
+    /**
+     * 复制文件夹到另一个文件夹
+     * @param string $source 源文件夹
+     * @param string $dest   目标文件夹
+     */
+    private function copydirs($source, $dest)
+    {
+        if (!is_dir($dest)) {
+            mkdir($dest, 0755, true);
+        }
+        foreach (
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::SELF_FIRST
+            ) as $item
+        ) {
+            if ($item->isDir()) {
+                $sontDir = $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
+                if (!is_dir($sontDir)) {
+                    mkdir($sontDir, 0755, true);
+                }
+            } else {
+                copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+            }
+        }
+    }
+}
