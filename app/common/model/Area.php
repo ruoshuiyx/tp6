b071df036b4c6140b66592021ca01f158c1fcd2e
zhaoyingjie <407593529@qq.com>
2021-07-30 17:33:16 +0800

    优化：优化区域模块

--- app/common/model/Area.php
+++ app/common/model/Area.php
@@ -28,7 +28,7 @@ namespace app\common\model;
 
 class Area extends Base
 {
-    // 开启自动写入时间戳字段
-    protected $autoWriteTimestamp = false;
-
+    // 定义时间戳字段名
+    protected $createTime = 'create_time';
+    protected $updateTime = 'update_time';
 }
